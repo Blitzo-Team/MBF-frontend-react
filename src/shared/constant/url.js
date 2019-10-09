760a03451e382fcfaf2786e0464617faaf3b767e
keith <keithlimmontajes@gmail.com>
2019-10-10 02:47:18 +0800

    add base url

--- src/shared/constant/url.js
+++ src/shared/constant/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:8000";
-
+// export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://frozen-brook-57894.herokuapp.com";
 export const API_BANNER_UPLOAD = `${BASE_URL}/api/slide`;
